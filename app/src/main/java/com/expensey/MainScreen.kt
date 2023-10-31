08ef6ca0b053c1b7b6453bb5557c576bb1bad572
Dante <Shashank996419@gmail.com>
2023-10-31 22:39:21 +0530

    Bottom navbar UI

--- app/src/main/java/com/expensey/MainScreen.kt
+++ app/src/main/java/com/expensey/MainScreen.kt
@@ -1,18 +1,29 @@
 package com.expensey
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
@@ -91,19 +102,35 @@ fun BottomNavigationItem(
     }
 }
 
-
-
-
 @Composable
 fun BottomNavigation(
-    content: @Composable () -> Unit) {
-    // You can define the layout and behavior for the entire BottomNavigation here.
-    // This may include rendering the individual BottomNavigationItems and any additional logic.
-    // For example, you can use a Row to arrange the items horizontally.
-    Row(
-        horizontalArrangement = Arrangement.SpaceAround,
-        modifier = Modifier.fillMaxWidth()
+    content: @Composable () -> Unit,
     ) {
-        content()
+    val colorScheme = MaterialTheme.colorScheme
+//    val backgroundColor = colorScheme.background
+
+    // Use the background color from the color scheme
+    val backgroundColor = if (isSystemInDarkTheme()) {
+        colorScheme.background
+    } else {
+        colorScheme.surface
+    }
+    Log.d("BackgroundColor", "Background color: $backgroundColor")
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+            .border(1.dp, Color.Gray, shape = RoundedCornerShape(16.dp))
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceAround,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(5.dp)
+                .clip(RoundedCornerShape(16.dp))
+                .background(backgroundColor)
+        ) {
+            content()
+        }
     }
 }

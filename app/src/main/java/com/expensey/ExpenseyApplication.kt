4b2764ed49dffdaa054f0f382bcf4eab59505125
Dante <Shashank996419@gmail.com>
2023-11-02 23:33:23 +0530

    Committing the ExpenseyApplication.kt Main Class

---
+++ app/src/main/java/com/expensey/ExpenseyApplication.kt
@@ -0,0 +1,19 @@
+package com.expensey
+
+import android.app.Application
+import androidx.room.Room
+import com.expensey.data.room.database.ExpenseyDatabase
+
+class ExpenseyApplication : Application() {
+	private lateinit var database: ExpenseyDatabase
+	override fun onCreate() {
+		super.onCreate()
+
+		val database = Room.databaseBuilder(this, ExpenseyDatabase::class.java, "expensey_database").build()
+	}
+
+	override fun onTerminate() {
+		super.onTerminate()
+		database.close()
+	}
+}

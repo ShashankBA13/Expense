b37565d8ae90ec0919d32e8d2cd7a32015fed628
Dante <Shashank996419@gmail.com>
2023-11-05 23:07:52 +0530

    Icon Alignment changes for the setitng screen

--- app/src/main/java/com/expensey/ui/screens/settings/SettingsScreen.kt
+++ app/src/main/java/com/expensey/ui/screens/settings/SettingsScreen.kt
@@ -1,9 +1,11 @@
 package com.expensey.ui.screens.settings
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -13,6 +15,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -32,43 +35,48 @@ fun SettingsScreen(navController : NavHostController) {
                 style = Typography.headlineLarge
             )
 
-            Row {
-                Column (
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Column(
                     modifier = Modifier.clickable {
                         navController.navigate("category")
-                    }
+                    },
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                 ) {
                     Icon(
                         imageVector = Icons.Outlined.Category,
                         contentDescription = "Edit Category", // Provide a content description as needed
                         modifier = Modifier
-                            .padding(start = 20.dp, end = 0.dp, top = 20.dp, bottom = 5.dp)
                             .size(40.dp)
                     )
                     Text(
                         text = "Category",
                         modifier = Modifier
-                            .padding(16.dp, 0.dp),
+                            .padding(16.dp, 5.dp),
                         style = Typography.bodyMedium
                     )
                 }
 
-                Column (
+                Column(
                     modifier = Modifier.clickable {
                         navController.navigate("accountsConfiguration")
                     },
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                 ) {
                     Icon(
                         imageVector = Icons.Outlined.AccountBalance,
-                        contentDescription = "Edit Category", // Provide a content description as needed
+                        contentDescription = "Edit Account", // Provide a content description as needed
                         modifier = Modifier
-                            .padding(start = 20.dp, end = 0.dp, top = 20.dp, bottom = 5.dp)
                             .size(40.dp)
                     )
                     Text(
                         text = "Accounts",
                         modifier = Modifier
-                            .padding(16.dp, 0.dp),
+                            .padding(16.dp, 5.dp),
                         style = Typography.bodyMedium
                     )
                 }

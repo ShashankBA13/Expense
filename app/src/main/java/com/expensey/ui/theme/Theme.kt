f3f2653c061811f4231e76ee861182649a85e2c6
Dante Alligheri <Shashank996419@gmail.com>
2024-02-28 11:40:36 +0530

    changed the status bar color to background

--- app/src/main/java/com/expensey/ui/theme/Theme.kt
+++ app/src/main/java/com/expensey/ui/theme/Theme.kt
@@ -61,13 +61,6 @@ fun ExpenseyTheme(
         else -> LightColorScheme
     }
     val view = LocalView.current
-//    if (!view.isInEditMode) {
-//        SideEffect {
-//            val window = (view.context as Activity).window
-//            window.statusBarColor = colorScheme.primary.toArgb()
-//            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-//        }
-//    }
 
     val systemUiController = rememberSystemUiController()
     if(darkTheme) {
